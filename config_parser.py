c85343db669297c7f2fc24516a8cf9ee135f26bd
PatzEdi <edwardferrari717@gmail.com>
2024-10-18 15:05:26 -0700

    Add project config and add some basic config tests to tests.py

---
+++ config_parser.py
@@ -0,0 +1,48 @@
+# Parser/loader of the configuration file
+import json
+def load_config():
+    """
+    Load the configuration from the 'proj_config.json' file.
+
+    Returns:
+        dict: The configuration data loaded from the JSON file.
+    """
+    with open("proj_config.json", "r") as file:
+        config = json.load(file)
+    return config
+
+def get_selected_template():
+    """
+    Retrieve the selected template name from the configuration.
+
+    Returns:
+        str: The name of the selected main template.
+    """
+    return load_config()["proj_settings"][0]["mainTemplate"]
+
+def get_main_template_data():
+    """
+    Get the data for the main template from the configuration.
+
+    Returns:
+        dict: The data corresponding to the main template.
+    """
+    return load_config()[get_selected_template()][0]
+
+def get_target_coords_amount():
+    """
+    Retrieve the 'random_seed' value from the main template data.
+
+    Returns:
+        int: The 'random_seed' value.
+    """
+    return get_main_template_data()["random_seed"]
+
+def get_print_data_info():
+    """
+    Retrieve the 'print_data_info' value from the main template data.
+
+    Returns:
+        Any: The 'print_data_info' value.
+    """
+    return get_main_template_data()["print_data_info"]
--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,41 @@
-# This file is for unit tests.
+import unittest
+import sys
+import os
+
+# File for unit tests...
+
+# We get the current directory of the script
+current_script_path = os.path.dirname(os.path.realpath(__file__)) 
+sys.path.append(os.path.join(current_script_path, "..")) # This is to find the proj_config.json file
+ 
+import config_parser
+
+class TestConfigParser(unittest.TestCase):
+    """ Class to test the config parser, e.g. test the types it returns """
+    def test_load_config(self):
+        """Test that the config is loaded and is a dictionary."""
+        config = config_parser.load_config()
+        self.assertIsInstance(config, dict)
+
+    def test_get_selected_template(self):
+        """Test that the selected template is a string."""
+        selected_template = config_parser.get_selected_template()
+        self.assertIsInstance(selected_template, str)
+
+    def test_get_main_template_data(self):
+        """Test that the main template data is a dictionary."""
+        main_template_data = config_parser.get_main_template_data()
+        self.assertIsInstance(main_template_data, dict)
+
+    def test_get_target_coords_amount(self):
+        """Test that the target coordinates amount is an integer."""
+        target_coords_amount = config_parser.get_target_coords_amount()
+        self.assertIsInstance(target_coords_amount, int)
+
+    def test_get_print_data_info(self):
+        """Test that the print data info is a boolean."""
+        print_data_info = config_parser.get_print_data_info()
+        self.assertIsInstance(print_data_info, bool)
+        
+if __name__ == '__main__':
+    unittest.main()
